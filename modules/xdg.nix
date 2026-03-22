77fca654ac0d4b9c6c89043d401815dc84c686d0
claim.second <claim.second.gmail.com>
2026-03-22 23:01:17 +0200

    Refactor xdg.nix to restructure portal service configuration

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,8 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  services.xdg.portal.enable = true;
-  services.xdg.portal.xdgOpenUsePortal = true;
-  services.xdg.portal.config.common.default = "*";
-  services.xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config.common.default = "*";
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 }
