2784f9ffa5921d2d508f7c10a1598ef90c636fab
claim.second <claim.second.gmail.com>
2026-04-18 23:19:35 +0300

    Remove niri from system packages in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -79,7 +79,6 @@
   environment.systemPackages = with pkgs; [
     #  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  git
-    niri
     nirius
     alacritty
     adwaita-icon-theme
