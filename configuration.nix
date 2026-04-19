62e0d22bdebd76a32eba8a7fa700861df520b1d2
claim.second <claim.second.gmail.com>
2026-04-19 12:44:08 +0300

    Remove dms-shell from the list of installed programs in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -80,7 +80,6 @@
     #  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  git
     niri
-    dms-shell
     nirius
     alacritty
     adwaita-icon-theme
