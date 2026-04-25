f70ce8a6729ebc48d87f4f8a6eafb718782dc85e
claim.second <claim.second.gmail.com>
2026-04-25 18:38:24 +0300

    Simplify noctalia-shell startup command by using string syntax instead of argv

--- modules/niri.nix
+++ modules/niri.nix
@@ -279,8 +279,7 @@
     spawn-at-startup "systemctl" "--user" "import-environment" "PATH" "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP"
     spawn-at-startup "dbus-update-activation-environment" "--systemd" "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP"
     spawn-at-startup "systemctl" "--user" "start" "graphical-session.target" 
-    spawn-at-startup {
-    argv [ "noctalia-shell" ]
+    spawn-at-startup "noctalia-shell"
     }
 
     hotkey-overlay {
