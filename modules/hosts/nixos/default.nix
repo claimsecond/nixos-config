10d5116afab2159b6106bc108fd9ce46976d6fc5
claim.second <claim.second.gmail.com>
2026-05-17 22:31:17 +0300

    feat: add cachix substituters to flake configuration and initialize host module structure

--- modules/hosts/nixos/default.nix
+++ modules/hosts/nixos/default.nix
@@ -4,7 +4,6 @@
 
 { inputs, config, ... }: {
   flake.nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
     specialArgs = { inherit inputs; };
     modules =
       # Все наши модули, собранные import-tree через flake.nixosModules.*
