eb66322873ac8547cfe5a0be4ec4828c1c99d8ae
claim.second <claim.second.gmail.com>
2026-06-21 09:23:37 +0300

    fix typo in import-tree comment in default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,6 @@
 # modules/default.nix
 # Корневой flake-parts модуль.
-# import-tree сканирует только эту директорию — здесь только flake-parts модули.
+# import-tree из flake.nixсканирует только эту директорию — здесь только flake-parts модули.
 
 { ... }: {
   systems = [ "x86_64-linux" ];
